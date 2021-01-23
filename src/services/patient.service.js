fd883d46d91fd5659c5bbc262d1188e05a659977
biancanfc <bianca.nunes1175@gmail.com>
2021-01-23 14:45:28 -0300

    add patient service

---
+++ src/services/patient.service.js
@@ -0,0 +1,40 @@
+const { getAll } = require('../controllers/patient.controller');
+const Patient = require('../models/patient');
+
+module.exports = {
+  async getAll({ search }) {
+    let query = {};
+
+    if (search) {
+      query = {
+        $or: [
+          {
+            name: new RegExp(`${search}`, 'i'),
+          },
+          {
+            cpf: new RegExp(`${search}`, 'i'),
+          },
+        ],
+      };
+    }
+
+    const patients = await Patient.find(query);
+
+    return patients;
+  },
+  async create(data) {
+    const patient = await Patient.create(data);
+
+    return patient;
+  },
+  async update(id, data) {
+    const patient = await Patient.findByIdAndUpdate(id, data, { new: true });
+
+    return patient;
+  },
+  async delete(id) {
+    const patient = await Patient.findByIdAndDelete(id);
+
+    return patient;
+  },
+};
